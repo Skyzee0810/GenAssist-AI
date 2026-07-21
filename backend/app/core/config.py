0a23507d281f6fb8d443f0d86617a3213412d502
Nafees Shah <nafeezshah35@gmail.com>
2026-07-22 00:34:33 +0530

    Connecting Postgres Database

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -12,6 +12,8 @@ class Settings(BaseSettings):
     debug: bool = True
     api_v1_prefix: str = "/api/v1"
 
+    database_url: str
+
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
